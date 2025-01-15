edb5518477c8625de144b70a875167930ba6e2cf
Armando Galeano <armando@maeztra.com>
2025-01-14 22:37:04 -0300

    fix

--- setup/initializeEnv.js
+++ setup/initializeEnv.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 updateEnvVariable(
   'DATABASE_URL',
-  `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.NODE_ENV === 'production' ? (process?.env?.SERVER_HOST ?? 'SERVER_HOST_NOT_DEFINED') : 'localhost'}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.NODE_ENV === 'production' ? (process?.env?.DB_HOST ?? 'DB_HOST not defined') : 'localhost'}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
 );
 
 updateEnvVariable(
   'SHADOW_DATABASE_URL',
-  `mysql://root:${process.env.DB_ROOT_PASSWORD}@${process.env.NODE_ENV === 'production' ? (process?.env?.SERVER_HOST ?? 'SERVER_HOST_NOT_DEFINED') : 'localhost'}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  `mysql://root:${process.env.DB_ROOT_PASSWORD}@${process.env.NODE_ENV === 'production' ? (process?.env?.DB_HOST ?? 'DB_HOST not defined') : 'localhost'}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
 );
